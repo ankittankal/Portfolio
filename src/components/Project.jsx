c7ee2775eee0ce5f5b4c933a0cca3ba2af2b1201
Ankit Tankal <ankittankal@gmail.com>
2024-07-20 18:25:57 +0530

    remove dark mode

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -4,16 +4,16 @@ import 'font-awesome/css/font-awesome.min.css';
 const Project = ({name , discription, ImageUrl, github, LiveUrl}) => {
   return (
     <div className='m-10'>
-        <div className="max-w-lg bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+        <div className="max-w-lg rounded-lg shadow bg-gray-800 border-gray-700">
             <a href="#">
                 <img className="rounded-t-lg w-[100%] h-auto " src={ImageUrl} alt="" />
             </a>
     
             <div className="p-5">
                 <a href="#">
-                    <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{name}</h5>
+                    <h5 className="mb-2 text-2xl font-bold tracking-tight text-white">{name}</h5>
                 </a>
-                <p className="mb-3 font-normal text-gray-700 dark:text-gray-400 pb-5">{discription}</p>
+                <p className="mb-3 font-normal text-gray-400 pb-5">{discription}</p>
     
                 <hr class="border-gray-700" />
                 <div className='flex justify-around pt-6'>
